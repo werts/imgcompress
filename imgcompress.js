5efd26cf4d8b6aaf615a72e9579a92c1dba013ab
werts <werts-we@qq.com>
2016-05-27 10:16:28 +0800

    a

--- imgcompress.js
+++ imgcompress.js
@@ -16,8 +16,8 @@
 				this.allFiles = this.fileFilter;
 				
 				//当文件数大于限制图片数时，直接返回
-				if (this.fileFilter.length > this.resstrictNum){
-					alert('不要上传大于限定的数量： ' + this.resstrictNum);
+				if (this.fileFilter.length > options.resstrictNum){
+					alert('不要上传大于限定的数量： ' + options.resstrictNum);
 					return;
 				}
 				//为文件添加索引
